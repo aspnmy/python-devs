38a9e888ba584596eb20c1247f8ab940ec1b2092
Abhilash Raj <raj.abhilash1@gmail.com>
2021-02-20 20:13:00 +0000

    Use github API to find versions instead of Downloads page.
    This uses Github API to fetch all tags and use the tagging pattern of
    vMAJOR.MINOR.PATCH[PRE_RELEASE] style to find all the version tags and then
    filter them based on the series we are tracking.
    
    This has an added advantage that we can get even pre-releases for series' that
    isn't yet release like 3.10 right now.

--- get_versions.py
+++ get_versions.py
@@ -1,26 +1,29 @@
 #! /usr/bin/env python3
 import os
+import json
 import urllib.request
-from bs4 import BeautifulSoup
 from packaging import version
 
-SERIES = ['2.7', '3.5', '3.6', '3.7', '3.8', '3.9']
+SERIES = ['2.7', '3.5', '3.6', '3.7', '3.8', '3.9', '3.10']
 
 
-def get_release_page():
-    with urllib.request.urlopen('http://python.org/downloads') as response:
-        html = response.read()
-    return html
+def get_tags_from_github():
+    with urllib.request.urlopen(
+            'https://api.github.com/repos/python/cpython/git/refs/tags'
+    ) as response:
+        val = response.read()
+    res = json.loads(val)
+    return res
 
 
-def get_all_versions(html):
-    soup = BeautifulSoup(html, features="html.parser")
-    versions = soup.find_all('span', class_='release-number')
-    versions = (version.parse(v.get_text().split()[-1])
-                for v in versions
-                if v.get_text() != 'Release version')
-    return list(versions)
-
+def get_version_from_tags(tags):
+    versions = []
+    for item in tags:
+        if item.get('ref').startswith('refs/tags/v'):
+            versions.append(
+                version.parse(item.get('ref').replace('refs/tags/v', ''))
+                )
+    return versions
 
 def get_latest_version(all_versions):
     # mapping from series to latest.
@@ -35,14 +38,23 @@ def get_latest_version(all_versions):
     return {key: value for key, value in latest.items() if key in SERIES}
 
 def main():
-    html = get_release_page()
-    all_versions = get_all_versions(html)
+    # html = get_release_page()
+    # all_versions = get_all_versions(html)
+    gh_response = get_tags_from_github()
+    all_versions = get_version_from_tags(gh_response)
+    print(all_versions)
     latest_versions = get_latest_version(all_versions)
 
     with open('version.txt', 'w') as fd:
         for key, value in latest_versions.items():
             print(f'{key} Series: {value}')
-            print(value, file=fd)
+            if value.is_prerelease:
+                # pre-releases are under directory which skips the pre-release
+                # markers, so print the directory path.
+                print(f'{value} {value.major}.{value.minor}.{value.micro}',
+                      file=fd)
+            else:
+                print(value, file=fd)
 
 if __name__ == "__main__":
     main()
